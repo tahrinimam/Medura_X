f60e934fccb22878610d510cc58e40e3134d1c58
tahrinimam <tahrinimam04@gmail.com>
2026-09-16 13:16:07 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.json({ message: "MeduraX API Running" });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
